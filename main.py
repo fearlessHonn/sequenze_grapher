1d354a0ec66f9d554aa3b5e4dd4f499ce1880ed5
fearlessHonn <henninglulei@gmail.com>
2021-11-18 22:09:13 +0100

    added default arguments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from get_sequence_from_string import get_sequence_from_string
 
 parser = argparse.ArgumentParser(description='Get sequence from string')
 parser.add_argument('-s', '--string', help='String to get sequence from', required=True, type=str)
-parser.add_argument('-r', '--range', help='The origin range of the sequence', required=False, type=int)
-parser.add_argument('-p', '--plot', help='If True, the sequence will be plotted', required=False, type=bool)
+parser.add_argument('-r', '--range', help='The origin range of the sequence', required=False, type=int, default=100)
+parser.add_argument('-p', '--plot', help='If True, the sequence will be plotted', required=False, type=bool, default=True)
 parser.parse_args()
 
 get_sequence_from_string(sequence=parser.parse_args().string, origin_range=parser.parse_args().range,
